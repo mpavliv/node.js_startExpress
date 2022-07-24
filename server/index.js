13488ab1ef25833a6b4f425605dc46711eebc93c
Mykola <mpavliv@ukr.net>
2022-07-24 22:55:30 +0300

    pre finished

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express')
+bodyParser = require('body-parser')
 const PORT = 8080;
+let sn;
+sn = new Set()
 
 const app = express();
+app.use(express.urlencoded({ extended: true, }));
+app.use(bodyParser.text({type: 'text/plain'}))
+  
+//app.use(express.json());
+
 
 app.get('/sn', (req, res) => {
-    res.send('responce serial nomber');
+    res.send(Array.from(sn));
 })
 
 app.post('/sn', (req, res) => {
-    if (!req.body) return response.sendStatus(400);
-    console.log(request.body);
+    if (!req.body) {
+        console.log('no body in require');
+        return res.sendStatus(400);
+    }
+    receivedData = req.body;
+    sn.add(req.body) 
     res.sendStatus(200);
 })
 

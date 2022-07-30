e3ed1b8245e3cc1697454a5b680b4e14ddc89c5a
Mykola <mpavliv@ukr.net>
2022-07-30 11:54:40 +0300

    30/07/22

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-bodyParser = require('body-parser');
+//bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 snBase = require('./sn_db_controller');
 
 const PORT = 8080;
 
 const app = express();
-app.use(express.urlencoded({ extended: true, }));
-app.use(bodyParser.text({type: 'text/plain'}))
-  
+// app.use( bodyParser.json({limit: '50mb'}) );
+// //app.use(bodyParser.text({type: 'text/plain', limit: '50mb'}))
+// app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 //app.use(express.json());
+app.use(fileUpload());
+
+
 snBase.deleteTable();
 snBase.createTable('sn');
 
@@ -24,6 +28,18 @@ app.post('/sn', (req, res) => {
     }
     receivedData = req.body;
     snBase.addSN(req.body);
+    res.sendStatus(200);
+})
+
+
+app.post('/fileSn', (req, res) => {
+    console.log(fileSn);
+    if (!req.body) {
+        console.log('no body in require');
+        return res.sendStatus(400);
+    }
+    receivedData = req.body;
+    console.log(receivedData);
     res.sendStatus(200);
 })
 
